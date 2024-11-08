26d105ee81c194c1da34a71a9df87128b50985a5
ffdadalto <ffdadalto@gmail.com>
2024-11-07 22:57:20 -0300

    ajustes

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,18 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import App from './App.vue';
+import Aura from '@primevue/themes/aura';
 
-createApp(App).mount('#app')
+import '@/style.css';
+import '@/assets/layout/bootstrap/css/bootstrap.min.css';
+
+const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            darkModeSelector: 'light'
+        }
+    }
+});
+app.mount('#app');
