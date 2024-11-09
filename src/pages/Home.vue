32f6b10e64c3f1a456907f24401bf8601b199bb3
ffdadalto <ffdadalto@gmail.com>
2024-11-09 18:04:53 -0300

    versao

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -14,7 +14,7 @@ const meses = ref<number>(12);
 const freqJuros = ref<string>('M');
 
 const versao = ref<{ major: number, minor: number, revision: number }>(
-    { major: 0, minor: 5, revision: 0 }
+    { major: 0, minor: 6, revision: 0 }
 );
 
 const rentCDB = ref<number>(13);
